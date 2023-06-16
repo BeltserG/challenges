37ea274436ff25db038bb61bf599da57e3736e1c
BeltserG <beltserg@icloud.com>
2023-06-16 18:17:15 +0500

    Markup + Script(only map)

---
+++ ip-address-tracker-master/script.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([52.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

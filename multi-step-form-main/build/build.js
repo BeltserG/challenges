c3ea7addf13d5a4edd80d69ba187380f9f81768c
BeltserG <beltserg@icloud.com>
2023-07-10 16:28:25 +0500

    Minor fix

--- multi-step-form-main/build/build.js
+++ multi-step-form-main/build/build.js
@@ -1,6 +1,12 @@
 const cards = document.querySelectorAll('.form');
+const stepButtons  = document.querySelector('.step-buttons');
 
 for (let card of cards) {
-    if(card.getAttribute("data-page") !== "1")
-    card.style.display = 'none';
+    if(card.getAttribute("data-page") !== "1"){
+        card.style.display = 'none';
+    }
+}
+
+if (document.querySelector(".form[data-page='1']").display !== "none"){
+    stepButtons.querySelector(".step-buttons--back").style.display = "none";
 }

8887f39419669d40fed9a14f20302fdf8e0796ec
BeltserG <beltserg@icloud.com>
2023-07-11 19:29:31 +0500

    2,3 form-pages design

--- multi-step-form-main/build/build.js
+++ multi-step-form-main/build/build.js
@@ -2,11 +2,11 @@ const cards = document.querySelectorAll('.form');
 const stepButtons  = document.querySelector('.step-buttons');
 
 for (let card of cards) {
-    if(card.getAttribute("data-page") !== "1"){
+    if(card.getAttribute("data-page") !== "4"){
         card.style.display = 'none';
     }
 }
 
-if (document.querySelector(".form[data-page='1']").display !== "none"){
-    stepButtons.querySelector(".step-buttons--back").style.display = "none";
-}
+// if (document.querySelector(".form[data-page='1']").display !== "none"){
+//     stepButtons.querySelector(".step-buttons--back").style.display = "none";
+// }

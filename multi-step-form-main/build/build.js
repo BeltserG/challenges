0b6b2b131c191e91c138eba6dcc80f23cf185731
BeltserG <beltserg@icloud.com>
2023-07-12 20:40:57 +0500

    Mobile design completed

--- multi-step-form-main/build/build.js
+++ multi-step-form-main/build/build.js
@@ -1,10 +1,20 @@
 const cards = document.querySelectorAll('.form');
 const stepButtons  = document.querySelector('.step-buttons');
-
+let currentStep = "5";
 for (let card of cards) {
-    if(card.getAttribute("data-page") !== "4"){
-        card.style.display = 'none';
+    switch (currentStep){
+        case "5":
+            document.querySelector(".step-buttons").style.display = "none";
+
+
     }
+    if(card.getAttribute("data-page") !== currentStep){
+        card.style.display = 'none';
+    }else{
+        document.querySelector(".step-buttons--next").style.backgroundColor = "hsl(243, 100%, 62%)";
+        document.querySelector(".step-buttons--next").innerHTML = "Confirm";
+    }
+    
 }
 
 // if (document.querySelector(".form[data-page='1']").display !== "none"){
